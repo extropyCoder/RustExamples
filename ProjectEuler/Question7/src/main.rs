809a6bbfbbe71f35ef0b9c9b88fad9a7f98a544a
Laurence Kirk <laurence@extropy.io>
2016-08-29 10:14:02 +0100

    trying with arrays

--- ProjectEuler/Question7/src/main.rs
+++ ProjectEuler/Question7/src/main.rs
@@ -6,29 +6,69 @@
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
+
 fn main() {
     println!("Starting Euler 7 !");
-    let x = 0;
-    let vec_nums =  read_file();
+    let mut largest = 0;
+    let nums_array = read_file();
 
-    for i in &vec_nums {
-        println!("Number is  {}", i);
-    }
+//    let mut iter = vec_nums.iter().enumerate();
+//.fold(0, |sum, x| sum + x);
 
-println!("Largest product is {} " ,x);
+//        println!("Number is  {}", &vec_nums[ii]);
+//        if i+12 > &vec_nums.length(){
+//            break;
+//        }
+
+
+// for chunk in vec_nums.chunks(13) {
+//     println!("{:?}", chunk);
+// }
+
+let array: [u32; 1000] = read_file();
+
+
+for x in array.iter() {
+
+    print!("{} ", x);
+}
+
+println!("Largest product is {} " ,largest);
 
 }
 
-fn read_file()-> Vec<u32>{
+
+
+
+fn read_file()->[u32;1000]{
 
     let f = BufReader::new(File::open("./src/input.txt").expect("open failed"));
-    let mut v: Vec<u32> = vec![];
+
+    let mut xs: [u32; 1000] = [0; 1000];
+    let mut position = 0;
     for line in f.lines() {
-         for c in line.expect("lines failed").chars() {
-             v.push(c.to_digit(10).expect("Invalid Character ! "));
-             //println!("Character: {}", c);
+     for c in line.expect("lines failed").chars() {
+             xs[position] = c.to_digit(10).expect("Invalid Character ! ");
+             position +=1;
+
          }
      }
 
-     return v;
+     return xs;
 }
+
+
+
+// fn read_file()-> Vec<u32>{
+//
+//     let f = BufReader::new(File::open("./src/input.txt").expect("open failed"));
+//     let mut v: Vec<u32> = vec![];
+//     for line in f.lines() {
+//          for c in line.expect("lines failed").chars() {
+//              v.push(c.to_digit(10).expect("Invalid Character ! "));
+//              //println!("Character: {}", c);
+//          }
+//      }
+//
+//      return v;
+// }
