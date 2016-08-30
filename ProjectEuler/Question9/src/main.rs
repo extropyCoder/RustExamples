d7fd0202df4275b183875d67315d0c8cf320525f
laurence <laurence@extropy.io>
2016-08-30 12:54:25 +0100

    starting 9

---
+++ ProjectEuler/Question9/src/main.rs
@@ -0,0 +1,25 @@
+//Project Euler Question 9
+
+
+//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//a2 + b2 = c2
+//For example, 32 + 42 = 9 + 16 = 25 = 52.
+//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//Find the product abc.
+
+
+
+fn main() {
+    println!("Starting Euler 9 !");
+
+
+for a in 1..999 {
+for b in 1..999 {
+for c in 1..999 {
+
+}
+}
+}
+
+
+}
