fbec4e155f6b60d721e31daec2ef19eba99cca2c
laurence <laurence@extropy.io>
2016-09-06 15:44:57 +0100

    starting 13

---
+++ ProjectEuler/Question13/src/main.rs
@@ -0,0 +1,12 @@
+//Project Euler Question 13
+
+//Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+
+
+fn main() {
+    println!("Starting Euler 13 !");
+
+
+
+    }
