111feb0f11afe3dca1a0cab66a1e9454c7cefd84
Laurence Kirk <laurence@extropy.io>
2016-09-09 08:16:43 +0100

    array dimension problems

--- ProjectEuler/Question13/src/main.rs
+++ ProjectEuler/Question13/src/main.rs
@@ -7,27 +7,57 @@ use std::fs::File;
 
 fn main() {
     println!("Starting Euler 13 !");
-    read_file();
+    let mut answer : u32 = 0;
+    let big_array = read_file();
+
+let new_array = reduce_array(&big_array);
+
+    for zz in big_array.iter(){
+        println!("zz is {?:}",zz);
+    }
+
+    // for x in new_array.iter(){
+    //     println!("x is {:?}",x);
+    // }
+
 
 
     }
 
-    fn read_file()-> [[u32; 50];50] {
+    fn read_file()-> [[u32; 50];100] {
 
-        let f = BufReader::new(File::open("./src/numbers.txt").expect("open failed"));
-        let mut xs: [[u32; 50];50] =[[0;50];50];
+        let f = BufReader::new(File::open("./src/number.txt").expect("open failed"));
+        let mut xs: [[u32; 50];100] =[[0;50];100];
         //let mut xs: [u64; 1000] = [0; 1000];
         let mut position = 0;
         let mut line_number = 0;
         for line in f.lines() {
          for c in line.expect("lines failed").chars() {
+                //println!("reading line {}",line_number);
+                //println!("reading position {}",position);
                  xs[position][line_number] = c.to_digit(10).expect("Invalid Character ! ") as u32;
                  position +=1;
-
-
              }
              line_number+=1;
+             position = 0;
+             if line_number >=50{
+                 break;
+             }
          }
 
          return xs;
+    }
+
+    fn reduce_array(input : &[[u32; 50];100]) -> [[u32; 12];100] {
+        let mut output: [[u32; 12];100] =[[0;12];100];
+        let mut num = 0;
+        for x in input.iter() {
+            //println!("num is {}",num);
+            for y in 0 .. 11 {
+                output[num][y] = x[y];
+            }
+            num+=1;
+        }
+        return output;
+
     }
