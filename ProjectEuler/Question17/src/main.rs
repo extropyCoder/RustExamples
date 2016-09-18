13b4d5ba61a1b619a89642fa8a46e0515c288f96
Laurence Kirk <laurence@extropy.io>
2016-09-18 20:06:49 +0100

    starting 17

--- ProjectEuler/Question17/src/main.rs
+++ ProjectEuler/Question17/src/main.rs
@@ -9,4 +9,45 @@
 
     fn main() {
         println!("Starting Euler 17!");
+
+
+
+
         }
+
+
+    fn get_letter_count_tens(n:u16)->u16{
+        match n {
+            20 ... 29  => return 6,
+            30 ... 39  =>return 6,
+            40 ... 49  => return 5,
+            50 ... 59  => return 5,
+            60 ... 69  => return 5,
+            70 ... 79  => return 7,
+            80 ... 89  => return 6,
+            90 ... 99  => return 6,
+            _ => return 0,
+        }
+
+    }
+
+fn get_letter_count_teen(n:u16)->u16{
+    match n {
+        10 => return 3,
+        11 | 12  =>return 6,
+        15 | 16 => return 7,
+        13 | 14 | 18 | 19 =>return 8,
+        17   =>return 9,
+        _ => return 0,
+    }
+
+}
+
+fn get_letter_count_unit(n:u16)->u16{
+    match n {
+        1 | 2 | 6 => return 3,
+        4 | 5 | 9  =>return 4,
+        3 | 7 | 8  =>return 5,
+        _ => return 0,
+    }
+}
