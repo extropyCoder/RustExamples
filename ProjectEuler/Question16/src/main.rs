4a52d3429bb462f988a5c5b0ceb65c55bd71f37b
Laurence Kirk <laurence@extropy.io>
2016-09-18 21:18:52 +0100

    17 finished

--- ProjectEuler/Question16/src/main.rs
+++ ProjectEuler/Question16/src/main.rs
@@ -1,12 +1,31 @@
-//Project Euler Question 17
+//Project Euler Question 16
 
-//If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-//If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
-//NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+//2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//What is the sum of the digits of the number 2^1000?
 
+extern crate num;
+    use num::{BigInt, Zero};
+    use num::bigint::ToBigInt;
 
     fn main() {
-        println!("Starting Euler 17!");
+        println!("Starting Euler 16!");
+        let n = 2.to_bigint().unwrap();
+        let x = num::pow(n, 1000);
+        //println!("answer is {}",answer);
+        let nz = 123.to_bigint().unwrap();
+        println!("answer is {}",get_digit_sum(nz));
 
 
         }
+
+
+
+fn get_digit_sum(n:BigInt) -> BigInt {
+    let ten = 10.to_bigint().unwrap();
+    let mut answer = n;
+    if n > ten {
+        let n2 = n / ten;
+        let answer = n %  10.to_bigint().unwrap() + get_digit_sum(n2) ;
+    }
+    return answer;
+}
--- ProjectEuler/Question17/src/main.rs
+++ ProjectEuler/Question17/src/main.rs
@@ -10,13 +10,52 @@
     fn main() {
         println!("Starting Euler 17!");
 
+        let mut answer = 0;
 
+        // 0 to 100
+        let one_hundred_answer = calc_one_hundred();
+        println!("100  is {}",one_hundred_answer);
 
+        for x in 1 .. 10{
+            let hundred_prefix = get_letter_count_unit(x) + 7;
+            answer = answer + hundred_prefix;
+            println!("hundred_prefix is {}",hundred_prefix);
+            answer = answer + one_hundred_answer + ((hundred_prefix+3)*99);
+        }
 
+        answer = one_hundred_answer + answer + 11;
+        println!("Answer is {}",answer);
         }
 
 
-    fn get_letter_count_tens(n:u16)->u16{
+    fn calc_one_hundred()->u32{
+    let mut answer  = 0;
+    let mut units = 0;
+    let mut teens = 0;
+    let mut tens = 0;
+
+        for x in 1 .. 10 {
+            units  = units + get_letter_count_unit(x);
+
+        }
+        //println!("1 to 10  is {}",units);
+        for x in 10 .. 20 {
+            teens = teens + get_letter_count_teen(x);
+        }
+        //println!("10 to 20  is {}",teens);
+
+        for x in 20 .. 100 {
+            tens = tens + get_letter_count_tens(x) + get_letter_count_unit(x % 10);
+            //println!("x is {}", x);
+            //println!("tens is {}", get_letter_count_tens(x) + get_letter_count_unit(10 % x));
+        }
+        //println!("20 to 100  is {}", tens);
+
+        return units + teens + tens;
+    }
+
+
+    fn get_letter_count_tens(n:u32)->u32{
         match n {
             20 ... 29  => return 6,
             30 ... 39  =>return 6,
@@ -31,7 +70,7 @@
 
     }
 
-fn get_letter_count_teen(n:u16)->u16{
+fn get_letter_count_teen(n:u32)->u32{
     match n {
         10 => return 3,
         11 | 12  =>return 6,
@@ -43,7 +82,8 @@ fn get_letter_count_teen(n:u16)->u16{
 
 }
 
-fn get_letter_count_unit(n:u16)->u16{
+fn get_letter_count_unit(n:u32)->u32{
+    //println!("n is {}",n);
     match n {
         1 | 2 | 6 => return 3,
         4 | 5 | 9  =>return 4,
