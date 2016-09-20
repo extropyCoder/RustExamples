126cbd8514bd07d946907c716d3c4a2efa32dbf3
laurence <laurence@extropy.io>
2016-09-20 12:37:29 +0100

    fix swapped files

--- ProjectEuler/Question18/src/main.rs
+++ ProjectEuler/Question18/src/main.rs
@@ -1,93 +1,43 @@
-//Project Euler Question 17
+//Project Euler Question 18
 
 
-//If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
-//If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
-//NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+//By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+//   3
+//  7 4
+// 2 4 6
+//8 5 9 3
+
+//That is, 3 + 7 + 4 + 9 = 23.
+
+//Find the maximum total from top to bottom of the triangle below:
+
+//                          75
+//                         95 64
+//                       17 47 82
+//                     18 35 87 10
+//                    20 04 82 47 65
+//                  19 01 23 75 03 34
+//                 88 02 77 73 07 63 67
+//               99 65 04 28 06 16 70 92
+//             41 41 26 56 83 40 80 70 33
+//           41 48 72 33 47 32 37 16 94 29
+//         53 71 44 65 25 43 91 52 97 51 14
+//       70 11 33 28 77 73 17 78 39 68 17 57
+//     91 71 52 38 17 14 91 43 58 50 27 29 48
+//   63 66 04 68 89 53 67 30 73 16 69 87 40 31
+// 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
 
 
     fn main() {
-        println!("Starting Euler 17!");
-
-        let mut answer = 0;
-
-        // 0 to 100
-        let one_hundred_answer = calc_one_hundred();
-        println!("100  is {}",one_hundred_answer);
-
-        for x in 1 .. 10{
-            let hundred_prefix = get_letter_count_unit(x) + 7;
-            answer = answer + hundred_prefix;
-            println!("hundred_prefix is {}",hundred_prefix);
-            answer = answer + one_hundred_answer + ((hundred_prefix+3)*99);
-        }
-
-        answer = one_hundred_answer + answer + 11;
-        println!("Answer is {}",answer);
-        }
-
-
-    fn calc_one_hundred()->u32{
-    let mut answer  = 0;
-    let mut units = 0;
-    let mut teens = 0;
-    let mut tens = 0;
-
-        for x in 1 .. 10 {
-            units  = units + get_letter_count_unit(x);
+        println!("Starting Euler 18!");
 
         }
-        //println!("1 to 10  is {}",units);
-        for x in 10 .. 20 {
-            teens = teens + get_letter_count_teen(x);
-        }
-        //println!("10 to 20  is {}",teens);
-
-        for x in 20 .. 100 {
-            tens = tens + get_letter_count_tens(x) + get_letter_count_unit(x % 10);
-            //println!("x is {}", x);
-            //println!("tens is {}", get_letter_count_tens(x) + get_letter_count_unit(10 % x));
-        }
-        //println!("20 to 100  is {}", tens);
-
-        return units + teens + tens;
-    }
 
 
-    fn get_letter_count_tens(n:u32)->u32{
-        match n {
-            20 ... 29  => return 6,
-            30 ... 39  =>return 6,
-            40 ... 49  => return 5,
-            50 ... 59  => return 5,
-            60 ... 69  => return 5,
-            70 ... 79  => return 7,
-            80 ... 89  => return 6,
-            90 ... 99  => return 6,
-            _ => return 0,
-        }
 
-    }
-
-fn get_letter_count_teen(n:u32)->u32{
-    match n {
-        10 => return 3,
-        11 | 12  =>return 6,
-        15 | 16 => return 7,
-        13 | 14 | 18 | 19 =>return 8,
-        17   =>return 9,
-        _ => return 0,
-    }
-
-}
-
-fn get_letter_count_unit(n:u32)->u32{
-    //println!("n is {}",n);
-    match n {
-        1 | 2 | 6 => return 3,
-        4 | 5 | 9  =>return 4,
-        3 | 7 | 8  =>return 5,
-        _ => return 0,
-    }
 }
