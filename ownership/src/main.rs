75527fb7d2d125c18002d8064490a1ddc0f22420
Laurence Kirk <laurence@extropy.io>
2016-10-05 07:21:37 +0100

    latest

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    println!("ownership examples!");
+
+    // mutable reference
+    let mut x = 5;
+    {
+        let y = &mut x;
+        *y += 1;
+    }
+    println!("{}", x);
+
+
+}
+
+
+fn foo(){
+    let v = 1;
+
+    let v2 = v;
+
+    println!("v is: {}", v);
 }
