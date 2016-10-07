fe45de13fcd86c55f7a2005188c82c1de1dff2a0
Laurence Kirk <laurence@extropy.io>
2016-10-07 04:56:11 +0100

    hash map

--- ProjectEuler/Question21/src/main.rs
+++ ProjectEuler/Question21/src/main.rs
@@ -7,12 +7,21 @@
 
 //Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
 //If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
-//For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+//For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+// The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 //Evaluate the sum of all the amicable numbers under 10000.
 
+use std::collections::HashMap;
 
     fn main() {
         println!("Starting Euler 21!");
+        let mut pairs = HashMap::new();
+        for n in 1 .. 10000{
+            let x= get_sum_divisors(n);
+            pairs.insert(n,x);
+        }
+
+
 
 
 
